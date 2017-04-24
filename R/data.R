a686fc6081b61207428a7531a37f72822de57745
Matt Moramarco <mmoramarco@DataNerdPro.local>
2017-04-24 16:57:14 -0400

    Updating with working functions.

--- R/data.R
+++ R/data.R
@@ -1,13 +1,86 @@
-#' Storms.
+#' The Tropical Cyclone Extended Best Track Dataset
 #'
-#' Data on storms.
+#' @source Regional and Mesocale Meteorology Branch of Colorado
+#' State University. \url{http://rammb.cira.colostate.edu/research/tropical_cyclones/tc_extended_best_track_dataset/}
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{storm_id}{Storm ID.}
+#'  \item{storm_name}{Storm name.}
+#'  \item{month}{Month.}
+#'  \item{day}{Day.}
+#'  \item{hour}{Hour.}
+#'  \item{year}{Year.}
+#'  \item{latitude}{Latitude.}
+#'  \item{longitude}{Longitude.}
+#'  \item{max_wind}{Maximum wind speed.}
+#'  \item{min_pressure}{Minimum pressure.}
+#'  \item{rad_max_wind}{Radius of maximum wind.}
+#'  \item{eye_diameter}{Eye diamter.}
+#'  \item{pressure_1}{Pressure 1.}
+#'  \item{pressure_2}{Pressure 2.}
+#'  \item{radius_34_ne}{Radius.}
+#'  \item{radius_34_se}{Radius.}
+#'  \item{radius_34_sw}{Radius.}
+#'  \item{radius_34_nw}{Radius.}
+#'  \item{radius_50_ne}{Radius.}
+#'  \item{radius_50_se}{Radius.}
+#'  \item{radius_50_sw}{Radius.}
+#'  \item{radius_50_nw}{Radius.}
+#'  \item{radius_64_ne}{Radius.}
+#'  \item{radius_64_se}{Radius.}
+#'  \item{radius_64_sw}{Radius.}
+#'  \item{radius_64_nw}{Radius.}
+#'  \item{storm_type}{Storm type.}
+#'  \item{distance_to_land}{Distance to land.}
+#'  \item{final}{Final.}
+#' }
+#' @examples
+#' \dontrun{
+#'  ext_tracks
+#' }
+"ext_tracks"
+
+#' A Single Observation of Hurricane Ike
 #'
-#' @format A data frame with five variables: \code{storm_id}, \code{storm_name},
-#'   \code{month}, \code{day}, \code{hour}, \code{year}, \code{latitude},
-#'   \code{longitude}, \code{max_wind}, \code{min_pressure}, \code{rad_max_wind},
-#'   \code{eye_diameter}, \code{pressure_1}, \code{pressure_2}, \code{radius_34_ne},
-#'   \code{radius_34_se}, \code{radius_34_sw}, \code{radius_34_nw}, \code{radius_50_ne},
-#'   \code{radius_50_se}, \code{radius_50_sw}, \code{radius_50_nw}, \code{radius_64_ne},
-#'   \code{radius_64_se}, \code{radius_64_sw}, \code{radius_64_nw}, \code{storm_type},
-#'   \code{distance_to_land}, and \code{final}
-"storms"
+#' @source Regional and Mesocale Meteorology Branch of Colorado
+#' State University. \url{http://rammb.cira.colostate.edu/research/tropical_cyclones/tc_extended_best_track_dataset/}
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{storm_id}{Storm Name and Year}
+#'  \item{date}{Date.}
+#'  \item{latitude}{Latitude.}
+#'  \item{longitude}{Longitude.}
+#'  \item{wind_speed}{Wind Speed}
+#'  \item{ne}{Radius of wind speed.}
+#'  \item{nw}{Radius of wind speed.}
+#'  \item{se}{Radius of wind speed.}
+#'  \item{sw}{Radius of wind speed.}
+#' }
+#' @examples
+#' \dontrun{
+#'  ike_observation
+#' }
+"ike_observation"
+
+#' A Single Observation of Hurricane Katrina
+#'
+#' @source Regional and Mesocale Meteorology Branch of Colorado
+#' State University. \url{http://rammb.cira.colostate.edu/research/tropical_cyclones/tc_extended_best_track_dataset/}
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{storm_id}{Storm Name and Year}
+#'  \item{date}{Date.}
+#'  \item{latitude}{Latitude.}
+#'  \item{longitude}{Longitude.}
+#'  \item{wind_speed}{Wind Speed}
+#'  \item{ne}{Radius of wind speed.}
+#'  \item{nw}{Radius of wind speed.}
+#'  \item{se}{Radius of wind speed.}
+#'  \item{sw}{Radius of wind speed.}
+#' }
+#' @examples
+#' \dontrun{
+#'  katrina_observation
+#' }
+"katrina_observation"
+
--- R/geom_hurricane.R
+++ R/geom_hurricane.R
@@ -1,9 +1,108 @@
+#' @title GeomHurricane
+#' @description
+#' This constructs the ggproto object necessary to display the hurricane radii
+#' plots.
+#' @import ggplot2
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate
+#' @importFrom dplyr left_join
+#' @importFrom dplyr group_by_
+#' @importFrom dplyr summarise
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr filter
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr bind_cols
+#' @importFrom dplyr rename
+#' @importFrom tidyr gather
+#' @importFrom grid polygonGrob
+#' @importFrom grid gpar
+#'
+#' @export
 GeomHurricane <- ggproto("GeomHurricane", Geom,
                          required_aes = c("x","y","r_ne","r_se","r_nw","r_sw"),
-                         default_aes = aes(scale_radii=1),
-                         draw_key = <a function used to draw the key in the legend>,
-                         draw_panel = function(data, panel_scales, coord) {
-                           ## Function that returns a grid grob that will
-                           ## be plotted (this is where the real work occurs)
-                           }
+                         default_aes = aes(scale_radii=1.0,
+                                           alpha=0.75),
+                         draw_key = draw_key_polygon,
+                         draw_group = function(data, panel_scales, coord) {
+
+                           angle_df <- data_frame(direction=rep(c("r_ne","r_se","r_sw","r_nw"),each=91),
+                                                  angle=c(360,1:90,90:180,180:270,270:360))
+
+                           data <-data %>%
+                             dplyr::select(x, y, r_ne, r_nw, r_se, r_sw, colour, fill,
+                                           PANEL, group, scale_radii, alpha) %>%
+                             tidyr::gather(direction, distance, -x, -y, -colour, -fill,
+                                           -PANEL, -group, -scale_radii, -alpha) %>%
+                             dplyr::mutate(distance = distance * 1852 * scale_radii) %>%
+                             dplyr::left_join(angle_df, by ="direction")
+
+                           origin_data <- data %>%
+                             dplyr::select(-distance) %>%
+                             dplyr::group_by_(.dots=setdiff(names(data),c("distance","angle"))) %>%
+                             dplyr::summarise(distance=0,angle=0) %>%
+                             dplyr::ungroup()
+
+                           data_r_ne <- data %>%
+                             dplyr::filter(direction=="r_ne") %>%
+                             dplyr::bind_rows(origin_data)
+                           data_r_nw <- data %>%
+                             dplyr::filter(direction=="r_nw") %>%
+                             dplyr::bind_rows(origin_data)
+                           data_r_se <- data %>%
+                             dplyr::filter(direction=="r_se") %>%
+                             dplyr::bind_rows(origin_data)
+                           data_r_sw <- data %>%
+                             dplyr::filter(direction=="r_sw") %>%
+                             dplyr::bind_rows(origin_data)
+                           data <- data_r_ne %>%
+                             dplyr::bind_rows(data_r_nw,data_r_se,data_r_sw)
+
+                           data <- data %>%
+                             dplyr::bind_cols(as.data.frame(geosphere::destPoint(cbind(data$x,data$y),
+                                                                                 b=data$angle,
+                                                                                 d=data$distance))) %>%
+                             dplyr::select(-x,-y) %>%
+                             dplyr::rename(x=lon,y=lat)
+
+                           coords <- coord$transform(data, panel_scales)
+
+                           grid::polygonGrob(
+                             x = coords$x,
+                             y = coords$y,
+                             gp = grid::gpar(col = coords$colour,
+                                       fill = coords$fill,
+                                       alpha = coords$alpha,
+                                       lty="solid", lwd=2)
+                            )
+                          }
                          )
+
+#' @title geom_hurricane
+#' @description
+#' Generates a radii plot on a ggmap object representing the maximum windspeed
+#' @examples
+#' \dontrun{
+#' get_map("Louisiana", zoom = 6, maptype = "toner-background", source="stamen",force=FALSE) %>%
+#'     ggmap(extent = "device") +
+#'     geom_hurricane(data = katrina_observation,
+#'                    aes(x = longitude, y = latitude,
+#'                        r_ne = ne, r_se = se, r_nw = nw, r_sw = sw,
+#'                        fill = wind_speed, color = wind_speed,
+#'                        scale_radii=0.75)) +
+#'     scale_color_manual(name = "Wind speed (kts)",
+#'                        values = c("red", "orange", "yellow")) +
+#'     scale_fill_manual(name = "Wind speed (kts)",
+#'                       values = c("red", "orange", "yellow"))
+#' }
+#' @import ggplot2
+#'
+#' @export
+geom_hurricane <- function(mapping = NULL, data = NULL, stat = "identity",
+                           position = "identity", na.rm = FALSE, show.legend = NA,
+                           inherit.aes = TRUE, ...) {
+  layer(
+    geom = GeomHurricane, mapping = mapping,  data = data, stat = stat,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
